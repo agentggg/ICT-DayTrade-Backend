a04fe05a9fd189fe772380b020c36360d4d78534
Tech & Faith <47467793+agentggg@users.noreply.github.com>
2026-01-05 15:00:37 -0500

    added url
    added url

--- Backend/ict/api/urls.py
+++ Backend/ict/api/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path("upload_flashcard", upload_flashcard, name="upload_flashcard"),
     path("upload_ai_flashcard", upload_ai_flashcard, name="upload_ai_flashcard"),
     path("get_ai_questions", get_ai_questions, name="get_ai_questions"),
-    
 ]
       
